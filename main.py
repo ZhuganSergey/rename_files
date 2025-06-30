ded8c346ec1604fdabdb1b592f04866932929e67
Serhii Zhuhan <sergey80ne@gmail.com>
2025-06-30 22:02:04 +0300

    v.2

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import os
 import re 
-from translitua import translit
+from translitua import translit as translit_ua
+from transliterate import translit as translit_ru
 
 inbox_path = './inbox'
 
 def has_cyrillic(text):
-    return bool(re.search('[а-яА-Я]', text))
+    return bool(re.search('[а-яА-Я,іІїЇєЄ]', text))
 
 def main():
     contents = os.listdir(inbox_path)
    
     for file in contents:
         if has_cyrillic(file):
-            new_file_name = translit(file)
-            os.rename(os.path.join(inbox_path, file), os.path.join(inbox_path, new_file_name))
+            filename_without_ua_symbols = translit_ua(file)
+            filename_without_ua_ru_symbols = translit_ru(filename_without_ua_symbols, language_code='ru', reversed=True)
+
+            os.rename(os.path.join(inbox_path, file), os.path.join(inbox_path, filename_without_ua_ru_symbols))
             
-            print(f'file: {file}, renamed to : {new_file_name}')
+            print(f'file: {file}, renamed to : {filename_without_ua_ru_symbols}')
             
 if __name__ == "__main__":
     main()
